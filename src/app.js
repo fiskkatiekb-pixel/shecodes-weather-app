2e3ca8241b6dd86e0b47494791d2bd81ef8ee07f
Katie <fiskkatie.kb@gmail.com>
2026-07-15 14:45:20 -0700

    working on api integration

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
+function currentWeather(response) {
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "3a2b7b79ct44f197028583982o0fd192";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(currentWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
