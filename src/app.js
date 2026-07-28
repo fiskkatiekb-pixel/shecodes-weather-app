2e2c9902425b67c42bd131360744dc56b7479b3e
Katie <fiskkatie.kb@gmail.com>
2026-07-28 13:55:47 -0700

    added time element

--- src/app.js
+++ src/app.js
@@ -5,12 +5,34 @@ function currentWeather(response) {
   let discriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+
+  timeElement.innerHTML = formateDate(date);
   discriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
   temperatureElement.innerHTML = Math.round(temperature);
+}
+function formateDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
